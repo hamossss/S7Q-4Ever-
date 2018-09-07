b795e8ef9183b4309af54f5d68e8968ee6f94659
hamossss <40631448+hamossss@users.noreply.github.com>
2018-09-08 01:29:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,4 +115,177 @@ client.on('ready', () => {
 });
 
 
+client.on("guildMemberAdd", (member) => {
+client.channels.get('470936933208686612').edit({name : `أعضاء السيرفر : ${member.guild.memberCount}`});
+})
+client.on("guildMemberRemove", (member) => {
+client.channels.get('470936933208686612').edit({name : `أعضاء السيرفر : ${member.guild.memberCount} `});
+})
+
+
+ client.on('message', async message => {
+            if(message.content.includes('discord.gg')){
+                if(message.member.hasPermission("MANAGE_GUILD")) return;
+        if(!message.channel.guild) return;
+        message.delete()
+          var command = message.content.split(" ")[0];
+    let muterole = message.guild.roles.find(`name`, "Muted");
+    if(!muterole){
+      try{
+        muterole = await message.guild.createRole({
+          name: "Muted",
+          color: "#000000",
+          permissions:[]
+        })
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(muterole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        });
+      }catch(e){
+        console.log(e.stack);
+      }
+    }
+           if(!message.channel.guild) return message.reply('** This command only for servers**');
+     message.member.addRole(muterole);
+    const embed500 = new Discord.RichEmbed()
+      .setTitle("Muted Ads")
+            .addField(`**  You Have Been Muted **` , `**تم اعطاك ميوت بسبب النشر**`)
+            .setColor("c91616")
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setAuthor(message.author.username, message.author.avatarURL)
+        .setFooter(`${message.guild.name} `)
+     message.channel.send(embed500)
+     message.author.send('` انت معاقب ميوت شاتي بسبب نشر سرفرات ان كان عن طريق الخطا **ف** تكلم مع الادارة `');
+   
+       
+    }
+})
+
+
+client.on('message', message => {
+     if (message.author.bot) return;
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription(" تم أرسال الرابط برسالة خاصة ")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription(" مدة الرابط : ساعه  عدد استخدامات الرابط : 1 ")
+      message.author.sendEmbed(Embed11)
+    }
+});
+
+client.on('message', message => {
+    if(message.content === 'الشعار'){
+        message.channel.send(':arrow_down:شعار دسكورد ')
+    }
+});
+
+
+client.on('message', message => {
+    if(message.content === 'الشعار'){
+        message.channel.send('``!S7Q |``')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === 'الشعار'){
+        message.channel.send(':arrow_down:شعار العبه')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === 'الشعار'){
+        message.channel.send('``ζ͡𝒮𝟳𝒬``')
+    }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'برب') {
+    msg.reply('**ٺــٻۧــټ ۖ ڀــٰ̍ا̍ ۛ ּحۡــڷــﯡۥ ۛ ּلآتـطــۄڸ ۗ::disappointed::broken_heart:**');
+  }
+});
+
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'متجر') {
+    msg.reply('**https://discord.gg/ge2KfDB تفضل يا حلو**');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'باك') {
+    msg.reply('** ۗ وّلّلّّكّكّممّّ نۨــۏڕٺ ۛ ּيۧ مۭــڗ ۛ ּ، ۛ ּمۭــنۨ ۖ ڙمۭــٰا̍نۨ ̨؏ــڼۨــک ۗ :rose::wave:**');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === '.') {
+    msg.reply('***أطلق اثبات وجود :hearts: ***');
+  }
+});
+
+let rebel;
+client.on("ready", async  => {
+    let guild = client.guilds.get("470653659114635275");
+  let users = guild.members.map(member => member.user.id);
+  let i;
+  rebel=0;
+for (i=0 ; i < users.length ; i++) {
+ let   check = guild.members.get(users[i]);
+if(!check.voiceChannelID){
+        continue;
+}else{
+  rebel++;
+}
+}
+guild.channels.find('id', '470893893219647489').setName("["+rebel+"]- عدد الاعضاء في الرومات");
+  client.setInterval(() =>{
+    let d = Date.now()
+  }, 5000);
+});
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let guild = client.guilds.get("470653659114635275");
+let newUserChannel = newMember.voiceChannel
+let oldUserChannel = oldMember.voiceChannel
+ if(oldUserChannel === undefined && newUserChannel !== undefined) {
+   rebel++;
+guild.channels.find('id', '470893893219647489').setName(" ["+rebel+"]- عدد الاعضاء في الرومات");
+} else if(newUserChannel === undefined){
+  rebel--;
+guild.channels.find('id', '470893893219647489').setName(" ["+rebel+"]- عدد الاعضاء في الرومات");
+}
+});
+client.on('message', Codes => {
+  
+  if(Codes.content === "!صوت") {
+      Codes.channel.send("["+rebel+"]- عدد الاعضاء في الرومات");
+}
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
